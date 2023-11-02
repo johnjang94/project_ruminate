39b55f4249008e8a75cdc1f63e96b0c1227ddca6
John Jang <jscolbe9675@gmail.com>
2023-11-02 10:28:39 -0400

    correcting code

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <footer className="bg-brand-green min-h-[600px] z-[60] border-brand-brown border-t-[24px] -mt-[5%] z-50 skew-y-0 p-16 py-32">
+    <footer className="bg-brand-green min-h-[600px] border-brand-brown border-t-[24px] -mt-[5%] z-50 skew-y-0 p-16 py-32">
       <div className="flex flex-col gap-6 text-neutral-50">
         <h1 className="text-6xl">Footer</h1>
         <p>z-index: 50</p>
